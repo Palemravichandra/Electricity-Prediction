3a425acf6f8c5c2f732182bc1a281343a904cb92
Palem Ravi chandra <123462175+Palemravichandra@users.noreply.github.com>
2023-06-03 14:31:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if select == 'STANDARD PARAMETERS':
         if submit:
             y = np.array([[Temp, Vacuum, Pressure, Humidity, TempDiff,PressureDiff,PowerPerFuel]])
             new = model.predict(y)
-    st.write("## : green[ELECTRICAL POWER OUTPUT IS]",round(new[0]))
+            st.write("## : green[ELECTRICAL POWER OUTPUT IS]",round(new[0]))
 else:
     col1, col2,col3= st.columns(3)
     with col1:
