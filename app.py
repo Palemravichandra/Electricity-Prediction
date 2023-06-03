2930099cece7c0050509db69d7aade17a2b00d34
Palem Ravi chandra <123462175+Palemravichandra@users.noreply.github.com>
2023-06-03 14:50:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,47 +11,48 @@ st.title(':orange[POWER GENERATION PREDICTOR]')
 view = ['STANDARD PARAMETERS','PREDICTION PARAMETERS']
 select = st.selectbox('Select Parameters',view)
 if select == 'STANDARD PARAMETERS':
-    col1,col2=st.columns(2)
+    col1,col2,col3=st.columns(3)
     with col1:
-        st.subheader(':violet[TEMPERATURE]')
-        Temp = st.selectbox('Select Temperature',df['Temp'].unique())
+        st.subheader(':violet[TEMP]')
+        Temp = st.selectbox('Select Temperature',df['temp'].unique())
         #st.text(" ")
         st.subheader(':violet[VACUUM]')
         Vacuum = st.selectbox('Select Vacuum',df['Vacuum'].unique())
         #st.text(" ")
         st.subheader(':violet[PRESSURE]')
         Pressure = st.selectbox('Select Pressure',df['Pressure'].unique())
+    with col2:
         st.subheader(':violet[HUMIDITY]')
         Humidity = st.selectbox('Select Humidity',df['Humidity'].unique())
-    with col2:
-        st.subheader(':violet[TEMPERATURE DIFFERENCE]')
+        st.subheader(':violet[Temp-Diff ]')
         TempDiff = st.selectbox('Select Temperature Difference',df['TempDiff'].unique())
         #st.text(" ")
-        st.subheader(':violet[PRESSURE DIFFERENCE]')
+        st.subheader(':violet[PRESSURE-DIFF]')
         PressureDiff = st.selectbox('Select Pressure Difference',df['PressureDiff'].unique())
-        st.subheader(':violet[POWER PER FUEL]')
+        #st.text(" ")
+    with col3:
+        st.subheader(':violet[POWER-PER-FUEL]')
         PowerPerFuel = st.selectbox('Select Power Per Fuel',df['PowerPerFuel'].unique())
         st.text(" ")
         st.text(" ")
-        st.text(" ")
         submit=st.button('SUBMIT')
-        if submit:
-            y = np.array([[Temp, Vacuum, Pressure, Humidity, TempDiff,PressureDiff,PowerPerFuel]])
-            new = model.predict(y)
-            st.write("## :green[ELECTRICAL POWER OUTPUT IS]",round(new[0]))
+    if submit:
+        y = np.array([[Temp, Vacuum, Pressure, Humidity, TempDiff,PressureDiff,PowerPerFuel]])
+        new = model.predict(y)
+        st.write(":green[ELECTRICAL POWER OUTPUT IS]",round(new[0]),'KWH')
 else:
-    col1, col2,col3= st.columns(3)
+    col1, col2,col3 = st.columns(3)
     with col1:
-        st.subheader(':violet[TEMPERATURE]')
+        st.subheader(':violet[TEMP]')
         Temp = st.text_input('Enter Temperature')
         if Temp:
             Temp=int(Temp)
-        #st.text(" ")
+        st.text(" ")
         st.subheader(':violet[VACUUM]')
         Vacuum = st.text_input('Enter Vacuum')
         if Vacuum:
             Vacuum=int(Vacuum)
-        #st.text(" ")
+        st.text(" ")
         st.subheader(':violet[PRESSURE]')
         Pressure = st.text_input('Enter Pressure')
         if Pressure:
@@ -61,26 +62,29 @@ else:
         Humidity = st.text_input('Enter Humidity')
         if Humidity:
             Humidity=int(Humidity)
-        st.subheader(':violet[TEMPERATURE DIFFERENCE]')
+
+        st.subheader(':violet[TEMP-DIFF]')
         TempDiff = st.text_input('Enter Temperature Difference')
         if TempDiff:
             TempDiff=int(TempDiff)
-        st.text(" ")
-        st.subheader(':violet[PRESSURE DIFFERENCE]')
+        #st.text(" ")
+        st.subheader(':violet[PRESSURE-DIFF]')
         PressureDiff = st.text_input('Enter Pressure Difference')
         if PressureDiff:
             PressureDiff=int(PressureDiff)
     with col3:
-        st.subheader(':violet[POWER PER FUEL]')
+        st.subheader(':violet[POWER-PER-FUEL]')
         PowerPerFuel = st.text_input('Enter Power Per Fuel')
         if PowerPerFuel:
             PowerPerFuel=int(PowerPerFuel)
         st.text(" ")
         st.text(" ")
+        st.text(' ')
         submit = st.button('SUBMIT')
-        if submit:
-            y = np.array([[Temp, Vacuum, Pressure, Humidity, TempDiff, PressureDiff, PowerPerFuel]])
-            new = model.predict(y)
-            st.write("## :green[ELECTRICAL POWER OUTPUT IS]", round(new[0]))
+    if submit:
+        y = np.array([[Temp, Vacuum, Pressure, Humidity, TempDiff, PressureDiff, PowerPerFuel]])
+        new = model.predict(y)
+        st.write("## :green[ELECTRICAL POWER OUTPUT IS]", round(new[0]),'KWH')
 
 st.write( f'<h5 style="color:rgb(0, 153, 153,0.35);">App Created by RAVI CHANDRA PALEM </h5>', unsafe_allow_html=True )
+
