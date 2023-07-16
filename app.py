6f7c6464e106830b2f4493798c83dacf891f549c
Palem Ravi chandra <123462175+Palemravichandra@users.noreply.github.com>
2023-07-16 16:47:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if select == 'STANDARD PARAMETERS':
     col1,col2,col3=st.columns(3)
     with col1:
         st.subheader(':violet[TEMP]')
-        Temp = st.selectbox('Select Temperature',sort(df['Temp'].unique()))
+        Temp = st.selectbox('Select Temperature',df['Temp'].unique().sort())
         #st.text(" ")
         st.subheader(':violet[VACUUM]')
         Vacuum = st.selectbox('Select Vacuum',df['Vacuum'].unique())
